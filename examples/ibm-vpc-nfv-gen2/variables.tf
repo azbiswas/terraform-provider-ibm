cf154df8bb9bab21b1557da38e8185a8af8d9a95
Amitabha Biswas <abiswas@us.ibm.com>
2019-11-07 20:08:15 -0800

    Test F5

---
+++ examples/ibm-vpc-nfv-gen2/variables.tf
@@ -0,0 +1,23 @@
+variable "zone1" {
+  default = "us-south-1"
+}
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "f5_image" {
+  default = "r006-648e7564-a7e1-40d5-8e92-6ff67c26ce9c"
+}
+
+variable "backend_image" {
+  default = "bf962ae4-4140-462b-8fa3-56fa1b49b06a"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+}
+
+variable "f5_license" {
+  default = ""
+}
