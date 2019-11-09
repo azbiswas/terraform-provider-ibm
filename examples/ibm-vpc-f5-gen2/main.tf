475f85c3b5e10abccfe70207bddc15b8380eab1c
Amitabha Biswas <abiswas@us.ibm.com>
2019-11-09 08:47:35 -0800

    IBM VPC Gen 2

---
+++ examples/ibm-vpc-f5-gen2/main.tf
@@ -0,0 +1,122 @@
+resource "ibm_is_vpc" "vpc1" {
+  name = "${var.vpc_name}"
+}
+
+resource "ibm_is_security_group" "sg1" {
+  name = "f5-bigip-1nic-demo-sg1"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+}
+
+resource "ibm_is_security_group_rule" "egress_all" {
+  depends_on = ["ibm_is_floating_ip.fip1"]
+  group      = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_rule1" {
+  depends_on = ["ibm_is_floating_ip.fip1"]
+  group      = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 8443
+    port_max = 8443
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_rule2" {
+  depends_on = ["ibm_is_floating_ip.fip1"]
+  group      = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_rule3" {
+  depends_on = ["ibm_is_floating_ip.fip1"]
+  group      = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_icmp_rule" {
+  depends_on = ["ibm_is_floating_ip.fip1"]
+  group      = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  icmp = {
+    code = 0
+    type = 8
+  }
+}
+
+resource "ibm_is_security_group_network_interface_attachment" "sgnic1" {
+  security_group    = "${ibm_is_security_group.sg1.id}"
+  network_interface = "${ibm_is_instance.ins1.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_subnet" "subnet1" {
+  name            = "f5-bigip-1nic-demo-subnet1"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone}"
+  ipv4_cidr_block = "10.240.0.0/24"
+}
+
+resource "ibm_is_ssh_key" "ssh1" {
+  name       = "${var.ssh_key_name}"
+  public_key = "${var.ssh_public_key}"
+}
+
+resource "ibm_is_instance" "ins1" {
+  name    = "${var.f5_vsi_name}"
+  image   = "${var.f5_image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.subnet1.id}"
+  }
+
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone}"
+  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  # user_data = "$(replace(file("f5-userdata.sh"), "F5-LICENSE-REPLACEMENT", var.f5_license)"
+
+  //User can configure timeouts
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
+}
+
+resource ibm_is_floating_ip "fip1" {
+  name   = "f5-bigip-1nic-demo-ip1"
+  target = "${ibm_is_instance.ins1.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_public_gateway" "gateway1" {
+  name = "f5-bigip-1nic-demo-gateway1"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone}"
+
+  //User can configure timeouts
+  timeouts {
+    create = "10m"
+  }
+}
---
+++ examples/ibm-vpc-f5-gen2/provider.tf
@@ -0,0 +1,21 @@
+#variable "ibmcloud_api_key" {
+#  default = ""
+#  description = "The APIKey of the IBM Cloud account where resources will be provisioned."
+#}
+
+variable "region" {
+  default = "us-south"
+  description = "The VPC Region that you want your VPC, networks and the F5 virtual server to be provisioned in. To list available regions, run `ibmcloud is regions`."
+}
+
+variable "generation" {
+  default = 2
+  description = "The VPC Generation to target. Valid values are 2 or 1."
+}
+
+provider "ibm" {
+#  ibmcloud_api_key      = "${var.ibmcloud_api_key}"
+  generation            = "${var.generation}"
+  region                = "${var.region}"
+  ibmcloud_timeout      = 300
+}
